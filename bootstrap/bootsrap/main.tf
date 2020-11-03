f79aa2a17a78ccf1c749663bcfc99dc8f3d7fbdb
gricasaws <labs.gc.aws@gmail.com>
2020-11-03 10:44:36 -0800

    terraform fmt

--- bootstrap/bootsrap/main.tf
+++ bootstrap/bootsrap/main.tf
@@ -4,7 +4,7 @@
 # --------------------------------------------------------------------------------------------
 
 terraform {
-    required_version = ">= 0.12"
+  required_version = ">= 0.12"
 }
 
 # ---------------------------------------------------------------------------------------------
@@ -12,11 +12,11 @@ terraform {
 # ---------------------------------------------------------------------------------------------
 
 provider "aws" {
-    region = "us-east-2"
+  region = "us-east-2"
 }
 
 resource "random_pet" "this_pet" {
-    length = 2
+  length = 2
 }
 
 # -----------------------------------------------------------------------------------------------
@@ -24,33 +24,33 @@ resource "random_pet" "this_pet" {
 # -----------------------------------------------------------------------------------------------
 
 resource "aws_s3_bucket" "terraform_state" {
-    bucket = "terraform-up-and-running-state-${random_pet.this_pet.id}"
+  bucket = "terraform-up-and-running-state-${random_pet.this_pet.id}"
 
-    # Prevent accidental deletion of this S3 bucket
+  # Prevent accidental deletion of this S3 bucket
 
-    lifecycle {
-        prevent_destroy = true
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  # Enable versioning so we can see the full revision history of our
+  # state files
+  versioning {
+    enabled = true
+  }
+
+  # Enable server-side encryption by default 
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
     }
-
-    # Enable versioning so we can see the full revision history of our
-    # state files
-    versioning {
-        enabled = true
-    }
-
-    # Enable server-side encryption by default 
-    server_side_encryption_configuration {
-        rule {
-            apply_server_side_encryption_by_default {
-                sse_algorithm = "AES256"
-            }
-        }
-    }
+  }
 }
 
 output "this_bucket_id" {
-    value = aws_s3_bucket.terraform_state.id
-    description = "The bucket_id output"
+  value       = aws_s3_bucket.terraform_state.id
+  description = "The bucket_id output"
 }
 
 ####################################################################################
@@ -59,14 +59,14 @@ output "this_bucket_id" {
 ####################################################################################
 
 resource "aws_dynamodb_table" "terraform_locks" {
-    name = "terraform-up-and-running-locks" 
-    billing_mode = "PAY_PER_REQUEST"
-    hash_key = "LockID"
+  name         = "terraform-up-and-running-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
 
-    attribute {
-        name = "LockID"
-        type = "S"
-    }
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
 }
 
 
