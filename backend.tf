5d1701e0a78c9e187dda6f9e8d4c397df5b6dcf5
gricasaws <labs.gc.aws@gmail.com>
2020-11-03 14:47:37 -0800

    removed /

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = "~> 0.12.0"
   backend "s3" {
     bucket = "terraform-up-and-running-state-superb-gelding"
-    key    = "/s3/jenkins/terraform.tfstate"
+    key    = "s3/jenkins/terraform.tfstate"
     region = "us-east-2"
   }
 }
