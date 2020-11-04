fd95f514cea1ea06b9c8484dbdfbb83b0b232be3
gricasaws <labs.gc.aws@gmail.com>
2020-11-03 17:27:58 -0800

    refreshing version

--- backend.tf
+++ backend.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = "~> 0.13.0"
+  required_version = ">= 0.13.0"
   backend "s3" {
     bucket = "terraform-up-and-running-state-superb-gelding"
     key    = "s3/jenkins/terraform.tfstate"
